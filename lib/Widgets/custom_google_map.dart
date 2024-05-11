5bd5af06e21e2edb1028916630c2f5d17ad4002a
ElSanteel <alsantel.ma@gmail.com>
2024-05-11 19:31:29 +0300

    dealing with polygons part 1

--- lib/Widgets/custom_google_map.dart
+++ lib/Widgets/custom_google_map.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 // import 'package:flutter/services.dart';
-import 'package:google_map_course/models/place_model.dart';
+// import 'package:google_map_course/models/place_model.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 // import 'dart:ui' as ui;
 
@@ -14,8 +14,9 @@ class CustomGoogleMap extends StatefulWidget {
 class _CustomGoogleMapState extends State<CustomGoogleMap> {
   late CameraPosition initialCameraPosition;
   late GoogleMapController googleMapController;
-  Set<Marker> markers = {};
-  Set<Polyline> polyLines = {};
+  // Set<Marker> markers = {};
+  // Set<Polyline> polyLines = {};
+  Set<Polygon> polygons = {};
 
   @override
   void initState() {
@@ -27,9 +28,11 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
     );
 
     // preferred to initialize markers in initMarkers method in the initState
-    initMarkers();
+    // initMarkers();
 
-    initPolyLines();
+    // initPolyLines();
+
+    initPolygons();
     super.initState();
   }
 
@@ -43,10 +46,12 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
   Widget build(BuildContext context) {
     return Stack(children: [
       GoogleMap(
-        polylines: polyLines,
         // to hide zoom controllers
         zoomControlsEnabled: false,
-        markers: markers,
+        // markers: markers,
+        // polylines: polyLines,
+        polygons: polygons,
+
         onMapCreated: (GoogleMapController controller) {
           // initialize google map controller
           googleMapController = controller;
@@ -84,23 +89,23 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
   //     setState(() {});
   //   }
 
-  // // method change size of marker icon
+  // method change size of marker icon
   // Future<Uint8List> getImageFromRawData(String image, double width) async {
-  //   // covert image to raw data format (Tharwat Explain this in the video)
-  //   // Load the image data from the specified asset (a marker icon in this case) into memory for further use.
+  // covert image to raw data format (Tharwat Explain this in the video)
+  // Load the image data from the specified asset (a marker icon in this case) into memory for further use.
   //   var imageData = await rootBundle.load('assets/images/icons8-marker-50.png');
 
-  //   // change the width of the image and return it as a byte data
-  //   // Instantiate an image codec from the image data buffer, specifying the target width. This is used to decode the image data into a frame.
+  // change the width of the image and return it as a byte data
+  // Instantiate an image codec from the image data buffer, specifying the target width. This is used to decode the image data into a frame.
   //   var imageCodec = await ui.instantiateImageCodec(
   //       imageData.buffer.asUint8List(),
   //       targetWidth: width.round());
 
-  //   // retrieve image frame information (Tharwat Explain this in the video)
-  //   // Retrieve the next frame from the image codec. This is used to get the decoded image data for rendering.
+  // retrieve image frame information (Tharwat Explain this in the video)
+  // Retrieve the next frame from the image codec. This is used to get the decoded image data for rendering.
   //   var imageFrameInfo = await imageCodec.getNextFrame();
 
-  //   // convert image to byte data but with modified properties
+  // convert image to byte data but with modified properties
   //   var imageByteData =
   //       await imageFrameInfo.image.toByteData(format: ui.ImageByteFormat.png);
 
@@ -108,69 +113,82 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
   // }
 
   // method to create multiple markers
-  void initMarkers() async {
-    // change marker icon if you will use getImageFromRawData method
+  // void initMarkers() async {
+  // change marker icon if you will use getImageFromRawData method
 
-    // var customMarkerIcon = BitmapDescriptor.fromBytes(
-    //     await getImageFromRawData('assets/images/icons8-marker-50.png', 70));
+  // var customMarkerIcon = BitmapDescriptor.fromBytes(
+  //     await getImageFromRawData('assets/images/icons8-marker-50.png', 70));
 
-    // change marker icon if you will use getImageFromRawData method
-    var customMarkerIcon = await BitmapDescriptor.fromAssetImage(
-        const ImageConfiguration(), 'assets/images/icons8-marker-50.png');
+  // change marker icon if you will use getImageFromRawData method
+  //   var customMarkerIcon = await BitmapDescriptor.fromAssetImage(
+  //       const ImageConfiguration(), 'assets/images/icons8-marker-50.png');
 
-    var myMarkers = places
-        .map((placeModel) => Marker(
-              icon: customMarkerIcon,
-              markerId: MarkerId(placeModel.id.toString()),
-              position: placeModel.latLng,
-              // this comment is put here to see the use of infoWindow
-              infoWindow: InfoWindow(title: placeModel.name),
-            ))
-        .toSet();
-    markers.addAll(myMarkers);
-    setState(() {});
-  }
+  //   var myMarkers = places
+  //       .map((placeModel) => Marker(
+  //             icon: customMarkerIcon,
+  //             markerId: MarkerId(placeModel.id.toString()),
+  //             position: placeModel.latLng,
+  // this comment is put here to see the use of infoWindow
+  //             infoWindow: InfoWindow(title: placeModel.name),
+  //           ))
+  //       .toSet();
+  //   markers.addAll(myMarkers);
+  //   setState(() {});
+  // }
 
-  void initPolyLines() {
-    Polyline polyLine = Polyline(
-      polylineId: const PolylineId('1'),
+  // void initPolyLines() {
+  //   Polyline polyLine = Polyline(
+  //     polylineId: const PolylineId('1'),
+  //     points: const [
+  //       LatLng(30.781647165030805, 30.99564275849909),
+  //       LatLng(30.78697008694028, 31.00022235705082),
+  //       LatLng(30.78459591541297, 31.004907874672455),
+  //       LatLng(30.780135938641447, 31.003634585857398),
+
+  //       // these points to try geodesic property
+  //       // LatLng(-28.718313313545117, 22.86984894027408),
+  //       // LatLng(81.73748455120459, 145.67510849968693)
+  //     ],
+  //     color: Colors.red,
+  //     startCap: Cap.roundCap,
+  //     width: 5,
+  //     zIndex: 2,
+  //     patterns: [PatternItem.dot, PatternItem.gap(10)],
+  //     geodesic: false,
+  //   );
+
+  //   Polyline polyLine2 = Polyline(
+  //     polylineId: const PolylineId('2'),
+  //     points: const [
+  //       LatLng(30.785253447644422, 31.000195661080703),
+  //       LatLng(30.78871166158042, 31.00148987292484)
+
+  // these points to try geodesic property
+  // LatLng(-28.718313313545117, 22.86984894027408),
+  // LatLng(81.73748455120459, 145.67510849968693)
+  //     ],
+  //     color: Colors.black,
+  //     startCap: Cap.roundCap,
+  //     width: 5,
+  //     zIndex: 1,
+  //     patterns: [PatternItem.dot, PatternItem.gap(10)],
+  //     geodesic: false,
+  //   );
+  //   polyLines.add(polyLine);
+  //   polyLines.add(polyLine2);
+  // }
+
+  void initPolygons() {
+    var polygon =  Polygon(
+      polygonId: const PolygonId('1'),
       points: const [
-        LatLng(30.781647165030805, 30.99564275849909),
-        LatLng(30.78697008694028, 31.00022235705082),
-        LatLng(30.78459591541297, 31.004907874672455),
-        LatLng(30.780135938641447, 31.003634585857398),
-
-        // these points to try geodesic property
-        // LatLng(-28.718313313545117, 22.86984894027408),
-        // LatLng(81.73748455120459, 145.67510849968693)
+        LatLng(30.788554991197426, 30.993810921834765),
+        LatLng(30.787964254798723, 31.012755694844166),
+        LatLng(30.77895507507975, 30.999724716676177)
       ],
-      color: Colors.red,
-      startCap: Cap.roundCap,
-      width: 5,
-      zIndex: 2,
-      patterns: [PatternItem.dot, PatternItem.gap(10)],
-      geodesic: false,
+      fillColor: Colors.black.withOpacity(0.5),
     );
-
-    Polyline polyLine2 = Polyline(
-      polylineId: const PolylineId('2'),
-      points: const [
-        LatLng(30.785253447644422, 31.000195661080703),
-        LatLng(30.78871166158042, 31.00148987292484)
-
-        // these points to try geodesic property
-        // LatLng(-28.718313313545117, 22.86984894027408),
-        // LatLng(81.73748455120459, 145.67510849968693)
-      ],
-      color: Colors.black,
-      startCap: Cap.roundCap,
-      width: 5,
-      zIndex: 1,
-      patterns: [PatternItem.dot, PatternItem.gap(10)],
-      geodesic: false,
-    );
-    polyLines.add(polyLine);
-    polyLines.add(polyLine2);
+    polygons.add(polygon);
   }
 }
 
